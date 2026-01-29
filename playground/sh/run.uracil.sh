415413446d4a12ff72c06071d2e629e0d64893e6
Zhen Peng <hi.pengzhen@gmail.com>
2026-01-29 01:26:18 +0000

    modified scripts for ExaChem

--- playground/sh/run.uracil.sh
+++ playground/sh/run.uracil.sh
@@ -7,9 +7,39 @@ cd "$OUTPUT_DIR"
 :> output.no_cxl.log
 :> output.with_cxl.log
 
-repeat=3
+repeat=1
+input_file="/home/exouser/pppp/project_exachem/exachem/inputs/ci/uracil.json"
+# input_file="/home/exouser/pppp/project_exachem/exachem/inputs/ozone.json"
 
 for i in $(seq 1 $repeat); do
-    bash ../../scripts/run_cross_instances.no_cxl.sh 2>&1 | tee -a output.no_cxl.log
-    bash ../../scripts/run_cross_instances.sh 2>&1 | tee -a output.with_cxl.log
+    # bash ../../scripts/run_cross_instances.no_cxl.sh 2>&1 | tee -a output.no_cxl.log
+    # bash ../../scripts/run_cross_instances.sh 2>&1 | tee -a output.with_cxl.log
+
+    ## No CXL
+    set -x
+    {
+    time \
+    mpirun -np 4 --hostfile /home/exouser/pppp/project_exachem/hostfiles/hostfile \
+        -mca btl_tcp_if_include enp1s0 \
+        -mca oob_tcp_if_include enp1s0 \
+        /home/exouser/local/install/tamm/bin/ExaChem "$input_file"
+    } 2>&1 | tee -a output.no_cxl.log
+    set +x
+
+    ## With CXL
+    set -x
+    {
+    time \
+    mpirun -np 4 --hostfile /home/exouser/pppp/project_exachem/hostfiles/hostfile \
+        -mca btl_tcp_if_include enp1s0 \
+        -mca oob_tcp_if_include enp1s0 \
+        -x CXL_MEMSIM_HOST=127.0.0.1 \
+        -x CXL_MEMSIM_PORT=9999 \
+        -x CXL_NUM_NODES=2 \
+        -x CXL_SIZE=21474836480 \
+        -x LD_PRELOAD=/home/exouser/pppp/OCEAN-private/build/libmpi_cxlmemsim_shim.so \
+        /home/exouser/local/install/tamm/bin/ExaChem "$input_file"
+    } 2>&1 | tee -a output.with_cxl.log
+    set +x
+
 done
--- scripts/run_cross_instances.no_cxl.sh
+++ scripts/run_cross_instances.no_cxl.sh
@@ -1,6 +1,6 @@
 set -eu
 
-set -x
+
 # mpirun -np 4 --oversubscribe \
 # -x CXL_MEMSIM_HOST=127.0.0.1 \
 # -x CXL_MEMSIM_PORT=9999 \
@@ -20,8 +20,9 @@ set -x
 # input_file="/home/exouser/pppp/project_exachem/exachem/inputs/ozone.json"
 input_file="/home/exouser/pppp/project_exachem/exachem/inputs/ci/uracil.json"
 
+set -x
 time \
-mpirun -np 32 --hostfile /home/exouser/pppp/project_exachem/hostfiles/hostfile \
+mpirun -np 4 --hostfile /home/exouser/pppp/project_exachem/hostfiles/hostfile \
     -mca btl_tcp_if_include enp1s0 \
     -mca oob_tcp_if_include enp1s0 \
     /home/exouser/local/install/tamm/bin/ExaChem "$input_file"
